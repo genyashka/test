8e6f78bed406cc8c7d5905c157c1cd33ecd19fb3
genyashka <genyashkat@gmail.com>
2022-05-27 16:20:39 +0300

    3-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
+import Header from "./components/Header"
+
 function App() {
     return (
+        <>
+        <Header />
         <div className="conteiner">
             <h1>My App</h1>
-            <h2>Hello Dima</h2>
 
          </div> 
+         </>
     )
 }
 
