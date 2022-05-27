51759afc712ede0477ac0af9a0be8e147388d3b7
genyashka <genyashkat@gmail.com>
2022-05-27 23:34:21 +0300

    3-4

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData  = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla rem laboriosam esse recusandae at? Cupiditate vero aperiam ipsam labore atque, nam adipisci praesentium ullam nobis quos hic nostrum soluta eveniet.",
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla rem laboriosam esse recusandae at? Cupiditate vero aperiam ipsam labore atque, nam adipisci praesentium ullam nobis quos hic nostrum soluta eveniet.",
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla rem laboriosam esse recusandae at? Cupiditate vero aperiam ipsam labore atque, nam adipisci praesentium ullam nobis quos hic nostrum soluta eveniet.",
+    },
+ ]
+
+ export default FeedbackData
