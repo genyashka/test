a6a1592a5f84774c9b950cfb4086e369d6eb58a2
genyashka <genyashkat@gmail.com>
2022-05-27 20:29:06 +0300

    3-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App() {
     return (
         <>
         <Header />
         <div className="conteiner">
-            <h1>My App</h1>
+            <FeedbackItem />
 
          </div> 
          </>
